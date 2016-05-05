djsutherland/PowerTools

commit 4a223b2278f07de41409cdfc11ee35c4078b1191

update for new version of forums :)

ptv_helper/views/soon.py
@@ -68,8 +68,8 @@ def my_shows_next():
     last_and_next = {state: [] for state in TURF_STATES}
 
     if show_states:
-        key = lambda e: (e.show_id, e.show.forum_id, e.show.name)
-        for (showid, forum_id, showname), show_eps \
+        key = lambda e: (e.show_id, e.show.forum_id, e.show.url, e.show.name)
+        for (showid, forum_id, url, showname), show_eps \
                 in itertools.groupby(eps, key):
             # sort by date here instead of in sql, because dunno how to tell sql
             # to sort missing dates last
@@ -84,7 +84,7 @@ def my_shows_next():
             else:  # loop ended without finding something in future
                 next_ep = None
 
-            show_info = (forum_id, showname)
+            show_info = (forum_id, url, showname)
             last_and_next[show_states[showid]].append(
                 (show_info, last_ep, next_ep))
         last_and_next = {

get_tvdb_links.py
@@ -4,7 +4,6 @@ from peewee import fn
 import tvdb_api
 
 from ptv_helper.app import db
-from ptv_helper.helpers import forum_url
 from ptv_helper.models import Show
 
 
@@ -17,7 +16,7 @@ def match_tvdbs(interactive=True, **api_kwargs):
                          .where(Show.tvdb_ids == '(new)')
                          .order_by(fn.lower(Show.name).asc())):
 
-            print('\n', show.name, forum_url(show.forum_id))
+            print('\n', show.name, show.url)
             try:
                 tvdb_id = t[show.name]['id']
             except (tvdb_api.tvdb_shownotfound, tvdb_api.tvdb_userabort):

grab_shows.py
@@ -9,7 +9,6 @@ from peewee import fn
 
 from ptv_helper.app import db
 from ptv_helper.models import Show
-from ptv_helper.helpers import forum_url
 
 
 letter_pages = [
@@ -48,7 +47,7 @@ megashows = [
 all_pages = letter_pages + megashows
 
 forum_url_fmt = re.compile(r'http://forums.previously.tv/forum/(\d+)-.*')
-SiteShow = namedtuple('SiteShow', 'name forum_id topics posts')
+SiteShow = namedtuple('SiteShow', 'name forum_id url topics posts last_post')
 
 # populated as side-effect of get_site_show_list (gross)
 megashow_children = defaultdict(set)
@@ -60,22 +59,43 @@ def get_site_show_list():
         if mega:
             mega_id = forum_url_fmt.match(page).group(1)
 
-        for table in root.cssselect('table.ipb_table'):
-            if not table.attrib['summary'].startswith('Sub-forums within'):
-                continue
-            for tr in table.cssselect('tr'):
-                if tr.attrib['class'] == 'redirect_forum':
+        for forum_list in root.cssselect('.cForumList'):
+            for li in forum_list.cssselect('li[data-forumid]'):
+                if len(li.cssselect('.cForumIcon_redirect')) > 0:
                     continue
-                a = tr.cssselect('td.col_c_forum a')[0]
-                forum_id = forum_url_fmt.match(a.attrib['href']).group(1)
+
+                forum_id = li.attrib['data-forumid']
+                a, = li.cssselect('h4.ipsDataItem_title a')
                 name = a.text_content()
-                topics, posts = [
-                    int(s.text_content().replace(',', ''))
-                    for s in tr.cssselect('td.col_c_stats li span')]
+                url = a.attrib['href']
+
+                topics = 0  # doesn't seem to be available anymore
+                dts = li.cssselect('.ipsDataItem_stats dt')
+                if len(dts) == 1:
+                    posts = dts[0].text_content().strip()
+                    if posts.endswith('k'):
+                        posts = int(float(posts[:-1]) * 1000)
+                    else:
+                        posts = int(posts)
+                elif len(dts) == 0:
+                    posts = 0
+                else:
+                    s = "{} stats entry for {} - {}"
+                    raise ValueError(s.format(len(dts), name, page))
+
+                times = li.cssselect('time')
+                if len(times) == 0:
+                    last_post = None
+                elif len(times) == 1:
+                    last_post = times[0].attrib['datetime']
+                else:
+                    s = "{} time entries for {} - {}"
+                    raise ValueError(s.format(len(times), name, page))
 
                 if mega:
                     megashow_children[mega_id].add(forum_id)
-                yield SiteShow(unicode(name), unicode(forum_id), topics, posts)
+                yield SiteShow(unicode(name), unicode(forum_id), unicode(url),
+                               topics, posts, last_post)
 
 
 def merge_shows_list(show_dead=True):
@@ -96,8 +116,10 @@ def merge_shows_list(show_dead=True):
                         name=show.name,
                         tvdb_ids="(new)",
                         forum_id=show.forum_id,
+                        url=show.url,
                         forum_posts=show.posts,
                         forum_topics=show.topics,
+                        last_post=show.last_post,
                         needs_leads=show.posts + show.topics > 50,
                         # unlikely that this'll ever hit, but...
                     )
@@ -115,8 +137,15 @@ def merge_shows_list(show_dead=True):
                               file=sys.stderr)
                         db_show.name = show.name
 
+                    if db_show.url != show.url:
+                        m = "URL disagreement: '{0}' in db, changing to '{1}'."
+                        print(m.format(db_show.url, show.url),
+                              file=sys.stderr)
+                        db_show.url = show.url
+
                     db_show.forum_posts = show.posts
                     db_show.forum_topics = show.topics
+                    db_show.last_post = show.last_post
                     db_show.save()
 
                 else:
@@ -139,12 +168,13 @@ def merge_shows_list(show_dead=True):
         # dead shows
         if show_dead:
             seen_ids = list(seen_forum_ids)
-            unseen = Show.select().where(~(Show.forum_id << seen_ids))
-            s = '\n'.join(
-                '\t{} - {}'.format(show.name, forum_url(show.forum_id))
-                for show in unseen)
-            if s:
-                print("Didn't see the following shows:\n" + s, file=sys.stderr)
+            if seen_ids:
+                unseen = Show.select().where(~(Show.forum_id << seen_ids))
+                s = '\n'.join(
+                    '\t{} - {}'.format(show.name, show.url) for show in unseen)
+                if s:
+                    print("Didn't see the following shows:\n" + s,
+                          file=sys.stderr)
 
     finally:
         db.close()

ptv_helper/helpers.py
@@ -16,11 +16,6 @@ def strip_the(s):
     return s
 
 
-@app.template_filter()
-def forum_url(forum_id):
-    return 'http://forums.previously.tv/forum/{0}-'.format(forum_id)
-
-
 def tvdb_url(series_id):
     return 'http://thetvdb.com/?tab=series&id={0}'.format(series_id)
 

ptv_helper/models.py
@@ -31,8 +31,10 @@ class Meta(BaseModel):
 class Show(BaseModel):
     name = pw.TextField()
     forum_id = pw.IntegerField()
+    url = pw.TextField()
     forum_topics = pw.IntegerField()
     forum_posts = pw.IntegerField()
+    last_post = pw.DateTimeField()
     tvdb_ids = pw.TextField()
 
     gone_forever = pw.BooleanField(default=False)

ptv_helper/templates/eps_soon.html
@@ -13,7 +13,7 @@
     <ul class="shows">
       {% for ep in eps %}
         <li>
-          <a href="{{ ep.show.forum_id|forum_url }}">{{ ep.show.name }}</a>:
+          <a href="{{ ep.show.url }}">{{ ep.show.name }}</a>:
           S{{ ep.season_number }}
           E{{ ep.episode_number }}:
           {{ ep.name }}

ptv_helper/templates/genre-list.bbcode
@@ -1,4 +1,4 @@
 [b]{{ genre }}[/b]: {% for show in shows -%}
-[url="{{ show['forum_id']|forum_url }}"]{{ show['name'] }}[/url]
+[url="{{ show['url'] }}"]{{ show['name'] }}[/url]
 {%- if not loop.last %} | {% endif -%}
 {%- endfor %}

ptv_helper/templates/list_shows.html
@@ -6,7 +6,7 @@
     {% for show in shows %}
       <li>
         {{ show.name }}
-        (<a href="{{ show.forum_id|forum_url }}">forum</a>)
+        (<a href="{{ show.url }}">forum</a>)
         ({{ show.tvdb_ids | tvdb_links | safe }})
         {% if show.gone_forever %}<span style="color: #f22;">(canceled)</span>{% endif %}
         {% if not show.we_do_ep_posts %}(no ep posts){% endif %}

commit d5754c248c3aa0bee60df601390e8f5b97c0eb19

fix my-next sorting (broken in 4a223b227)

ptv_helper/views/soon.py
@@ -88,7 +88,7 @@ def my_shows_next():
             last_and_next[show_states[showid]].append(
                 (show_info, last_ep, next_ep))
         last_and_next = {
-           k: sorted(v, key=lambda inf: strip_the(inf[0][1]).lower())
+           k: sorted(v, key=lambda inf: strip_the(inf[0][2]).lower())
            for k, v in last_and_next.iteritems()
         }
 