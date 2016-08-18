2d3f3ca6c630620122bbb06702c4d4450e6674cb
Dougal J. Sutherland <dougal@gmail.com>
2016-08-18 14:49:13 -0400

    add reminder to override secret key

--- ptv_helper/config/default.py
+++ ptv_helper/config/default.py
@@ -3,6 +3,7 @@ import os
 DATABASE = 'sqlite:///' + os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../../ptv.db'))
 SECRET_KEY = '9Zbl48DxpawebuOKcTIxsIo7rZhgw2U5qs2mcE5Hqxaa7GautgOh3rkvTabKp'
+# ^ remember to override in deploy!
 ADMINS = ['dougal@gmail.com']
 
 DEBUG = True
