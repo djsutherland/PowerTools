51875f08cfa848ff810cc8fbc2685086e4398f5c
Dougal J. Sutherland <dougal@gmail.com>
2017-04-05 17:29:15 +0100

    update_tvdb script: allow updating specific serieses only

--- update_tvdb.py
+++ update_tvdb.py
@@ -1,5 +1,5 @@
 from ptv_helper.app import app
-from ptv_helper.tvdb import update_db
+from ptv_helper.tvdb import update_db, update_serieses
 
 
 def main(force=False, quiet=False):
@@ -8,10 +8,14 @@ def main(force=False, quiet=False):
     parser.add_argument('--force', '-f', action='store_true', default=False)
     parser.add_argument('--quiet', '-q', dest='verbose', action='store_false',
                         default=True)
+    parser.add_argument('ids', nargs='*', type=int)
     args = parser.parse_args()
 
     with app.app_context():
-        update_db(**vars(args))
+        if args.ids:
+            update_serieses(args.ids, verbose=args.verbose)
+        else:
+            update_db(force=args.force, verbose=args.verbose)
 
 
 if __name__ == '__main__':
