511477ea4a8f7aadbb802948ef0cc4b403770dca
Dougal J. Sutherland <dougal@gmail.com>
2018-02-27 21:00:20 +0000

    better clipboard handling (closes #39)

---
+++ ptv_helper/static/clipboard.js
@@ -0,0 +1,21 @@
+// based on https://clipboardjs.com/assets/scripts/tooltips.js
+function clearTooltip(e) {
+  e.currentTarget.setAttribute('class', 'btn');
+  e.currentTarget.removeAttribute('aria-label');
+}
+function showTooltip(elem, msg) {
+  elem.setAttribute('class', 'btn tooltipped tooltipped-s');
+  elem.setAttribute('aria-label', msg);
+}
+
+if (Clipboard.isSupported()) {
+  var clipboard = new Clipboard('.clip');
+  clipboard.on('success', function (e) {
+    showTooltip(e.trigger, 'Copied!');
+  });
+
+  $('.clip').on('mouseleave', clearTooltip);
+  $('.clip').on('blur', clearTooltip);
+} else {
+  $('.clip-parent').hide();
+}
