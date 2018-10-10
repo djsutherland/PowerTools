3fef1ddb3183179b8ce2529647a1498434adf9b7
Dougal J. Sutherland <dougal@gmail.com>
2018-10-10 17:59:17 +0100

    server.py: some basic CLI args

--- server.py
+++ server.py
@@ -7,4 +7,11 @@ import ptv_helper.views
 app = ptv_helper.app.app
 
 if __name__ == '__main__':
-    app.run()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5000)
+    g = parser.add_mutually_exclusive_group()
+    g.add_argument('--debug', action='store_true', default=True)
+    g.add_argument('--no-debug', action='store_false', dest='debug')
+    args = parser.parse_args()
+    app.run(**vars(args))
