f6e6521b3c5112495d407d4652c8c278f2eec2fe
Danica J. Sutherland <djs@djsutherland.ml>
2021-05-17 16:23:43 -0700

    show update time on index

--- powertools/views/__init__.py
+++ powertools/views/__init__.py
@@ -2,9 +2,10 @@ import datetime
 
 from flask import render_template, request, send_from_directory
 from peewee import fn
+from tzlocal import get_localzone
 
 from ..base import app
-from ..models import BingoSquare, Mod
+from ..models import BingoSquare, Meta, Mod
 
 from . import bingo, grab_shows, manage_users, match_tvdb, reports, soon, turfs
 
@@ -12,11 +13,14 @@ from . import bingo, grab_shows, manage_users, match_tvdb, reports, soon, turfs
 @app.route("/")
 def index():
     num_bingo = BingoSquare.select(fn.Max(BingoSquare.which)).scalar()
+    tz = get_localzone()
     return render_template(
         "index.html",
         now=datetime.datetime.now(),
         num_bingo=num_bingo,
         mods=Mod.select().order_by(Mod.name.asc()),
+        update_time=tz.localize(datetime.datetime.fromtimestamp(
+            float(Meta.get_value('forum_update_time', 0))))
     )
 
 
